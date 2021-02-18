a706284ed44c06ab4c21fab1809466724d3dded4
Rhett Harrison <reharri7@asu.edu>
2021-02-18 14:51:56 -0800

    Increased SEO and Accessibility

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({ searchfield, searchChange }) => {
     return(
         <div className='pa2'>
             <input 
+            aria-label= 'Search Robots'
             type='search' 
             placeholder='Search Robots'
             className='pa3 ba b--green bg-lightest-blue' 
